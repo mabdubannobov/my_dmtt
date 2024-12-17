cddaea68ba3df7af778a7fb72cc2f875c1573380
Muhammadqodir <mq.abdubannobov@gmail.com>
2024-12-17 15:48:37 +0500

    complete auth progress

--- lib/features/signin/screens/sign_in.dart
+++ lib/features/signin/screens/sign_in.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:my_dmtt/features/main_screen.dart';
+import 'package:my_dmtt/features/signin/bloc/sign_in_bloc.dart';
 
 import '../../../constants/app_assets.dart';
 import '../../../constants/app_colors.dart';
@@ -24,92 +27,115 @@ class _SignInScreenState extends State<SignInScreen> {
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
+        child: BlocConsumer<SignInBloc, SignInState>(
+          listener: (context, state) {
+            if (state is SignInLoadedState) {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => const MainScreen(),
+                  ),
+                  (Route route) => false);
+            } else if (state is SignInErrorState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: AppColors.error.withOpacity(0.2),
+                  elevation: 0,
+                  content: Text(
+                    'Login yoki parol xato kiritildi',
+                    style: AppTextStyles.regularStyle.copyWith(
+                      fontSize: 16,
+                      color: AppColors.error,
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is SignInLoadingState) {
+              return const Center(
+                child: CupertinoActivityIndicator(),
+              );
+            }
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(24, 16, 24, 48),
+                      child: GestureDetector(
+                        child: SvgPicture.asset(AppAssets.icons.arrowLeft),
+                      ),
+                    ),
+                  ],
+                ),
+                SvgPicture.asset(
+                  AppAssets.icons.appBarLogo,
+                  width: 120,
+                ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(24, 16, 24, 48),
-                  child: GestureDetector(
-                    child: SvgPicture.asset(AppAssets.icons.arrowLeft),
+                  padding: const EdgeInsets.symmetric(vertical: 48),
+                  child: Text(
+                    "Hisobga kirish",
+                    style: AppTextStyles.boldStyle.copyWith(
+                      fontSize: 32,
+                      height: 1.2,
+                      color: AppColors.greyscaleLight,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: SignInField(
+                    controller: userController,
+                    icon: AppAssets.icons.userField,
+                    hintText: 'Foydalanuvchi nomi',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(24, 16, 24, 24),
+                  child: SignInField(
+                    controller: passwordController,
+                    icon: AppAssets.icons.passwordField,
+                    hintText: 'Xavfsizlik paroli',
+                  ),
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    fixedSize: WidgetStatePropertyAll(Size.fromWidth(MediaQuery.of(context).size.width - 48)),
+                    backgroundColor: const WidgetStatePropertyAll(AppColors.disabledButton),
+                  ),
+                  onPressed: () {
+                    if (userController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+                      context.read<SignInBloc>().add(
+                            SignInEnterEvent(
+                              userController.text,
+                              passwordController.text,
+                            ),
+                          );
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      "Kirish",
+                      style: AppTextStyles.boldStyle.copyWith(
+                        color: Colors.white,
+                        fontSize: 16,
+                        letterSpacing: 0.2,
+                        height: 1.4,
+                      ),
+                    ),
                   ),
                 ),
               ],
-            ),
-            SvgPicture.asset(
-              AppAssets.icons.appBarLogo,
-              width: 120,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 48),
-              child: Text(
-                "Hisobga kirish",
-                style: AppTextStyles.boldStyle.copyWith(
-                  fontSize: 32,
-                  height: 1.2,
-                  color: AppColors.greyscaleLight,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: SignInField(
-                controller: userController,
-                icon: AppAssets.icons.userField,
-                hintText: 'Foydalanuvchi nomi',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 16, 24, 24),
-              child: SignInField(
-                controller: passwordController,
-                icon: AppAssets.icons.passwordField,
-                hintText: 'Xavfsizlik paroli',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                fixedSize: WidgetStatePropertyAll(Size.fromWidth(MediaQuery.of(context).size.width - 48)),
-                backgroundColor: const WidgetStatePropertyAll(AppColors.disabledButton),
-              ),
-              onPressed: () {
-                if (userController.text == "user" && passwordController.text == "user123") {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MainScreen()));
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: AppColors.error.withOpacity(0.2),
-                      elevation: 0,
-                      content: Text(
-                        'Login yoki parol xato kiritildi',
-                        style: AppTextStyles.regularStyle.copyWith(
-                          fontSize: 16,
-                          color: AppColors.error,
-                        ),
-                      ),
-                    ),
-                  );
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text(
-                  "Kirish",
-                  style: AppTextStyles.boldStyle.copyWith(
-                    color: Colors.white,
-                    fontSize: 16,
-                    letterSpacing: 0.2,
-                    height: 1.4,
-                  ),
-                ),
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_dmtt/features/main_screen.dart';
+import 'package:my_dmtt/features/signin/bloc/sign_in_bloc.dart';
 import 'package:my_dmtt/features/walkthrough/screens/splash.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'constants/sharedprefrences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var token = prefs.getString(Shared.accessToken);
+  runApp(MainApp(
+    home: token == null ? const SplashScreen() : const MainScreen(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({required this.home, super.key});
+
+  final dynamic home;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +31,18 @@ class MainApp extends StatelessWidget {
     ));
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return BlocProvider(
+      create: (context) => SignInBloc(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => SignInBloc()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: home,
+          theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+        ),
+      ),
     );
   }
 }
