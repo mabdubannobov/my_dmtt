3e1385a596b20952a019cbf58c1f0c2364c48122
Muhammadqodir <mq.abdubannobov@gmail.com>
2025-01-25 10:45:51 +0500

    add butto for create order

--- lib/features/cart/screens/cart.dart
+++ lib/features/cart/screens/cart.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_dmtt/features/home/widgets/empty_cart.dart';
 
 import '../../../constants/app_assets.dart';
+import '../../../constants/app_colors.dart';
 import '../../../constants/app_text_styles.dart';
 import '../../../models/product_model.dart';
 import '../widgets/cart_item.dart';
@@ -59,21 +60,70 @@ class _CartScreenState extends State<CartScreen> {
                 return const EmptyCart();
               }
 
-              return ListView.builder(
-                itemCount: box.length,
-                itemBuilder: (context, index) {
-                  final key = box.keyAt(index);
-                  final item = box.get(key);
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: box.length,
+                      itemBuilder: (context, index) {
+                        final key = box.keyAt(index);
+                        final item = box.get(key);
 
-                  return CartItem(
-                    itemKey: key,
-                    name: item.name,
-                    count: item.value,
-                    measure: item.measure,
-                    photoUrl: item.imageUrl,
-                    onDelete: () => _deleteItem(key),
-                  );
-                },
+                        return Column(
+                          children: [
+                            CartItem(
+                              itemKey: key,
+                              name: item.name,
+                              count: item.value,
+                              measure: item.measure,
+                              photoUrl: item.imageUrl,
+                              onDelete: () => _deleteItem(key),
+                            ),
+                            if (index != box.length - 1)
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 16),
+                                child: Divider(
+                                  color: AppColors.greyscaleLight.shade100,
+                                  thickness: 1.0,
+                                ),
+                              ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                          top: BorderSide(
+                            color: AppColors.greyscaleLight.shade100,
+                            width: 1.0,
+                          ),
+                        )),
+                    child: TextButton(
+                      style: ButtonStyle(
+                        fixedSize: WidgetStatePropertyAll(Size.fromWidth(MediaQuery.of(context).size.width - 32)),
+                        backgroundColor: WidgetStatePropertyAll(AppColors.primaryLight),
+                      ),
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Text(
+                          "Buyurtma berish",
+                          style: AppTextStyles.boldStyle.copyWith(
+                            color: Colors.white,
+                            fontSize: 16,
+                            letterSpacing: 0.2,
+                            height: 1.4,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               );
             },
           ),
