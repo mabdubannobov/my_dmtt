b271ecea4e7619ec738a3a30128b7b809dac30c9
Muhammadqodir <mq.abdubannobov@gmail.com>
2025-01-28 15:06:16 +0500

    update themmode when reopen app

--- lib/cubit/theme_cubit.dart
+++ lib/cubit/theme_cubit.dart
@@ -1,10 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
-  ThemeCubit() : super(ThemeMode.light);
+  ThemeCubit() : super(ThemeMode.light) {
+    _loadTheme();
+  }
 
-  void toggleTheme(bool isDarkMode) {
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    emit(isDarkMode ? ThemeMode.dark : ThemeMode.light);
+  }
+
+  void toggleTheme(bool isDarkMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', isDarkMode);
     emit(isDarkMode ? ThemeMode.dark : ThemeMode.light);
   }
 }
