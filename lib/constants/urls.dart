8cd7aa7a332a1026bb2f003cb316a67762cdf08b
Muhammadqodir Abdubannobov <mq.abdubannobov@gmail.com>
2025-08-13 17:44:20 +0500

    update login api url

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,1 +1,2 @@
 String baseURL = "http://api.mydmtt.uz";
+String baseURLNew = "https://mydmtt.uz";
--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -5,21 +5,23 @@ import '../constants/sharedprefrences.dart';
 import '../constants/urls.dart';
 
 class LoginService {
-  final Dio dio = Dio(BaseOptions(baseUrl: baseURL));
-  final String login = '/auth/login/';
+  final Dio dio = Dio(BaseOptions(baseUrl: baseURLNew));
+  final String login = '/api/token/';
 
-  Future<String> loginWithUserName({required String password, required String number}) async {
+  Future<String> loginWithUserName(
+      {required String password, required String number}) async {
     final result = await dio.post(
       login,
       data: {
         "username": number,
         "password": password,
+        "fcm_token": "string",
       },
     );
 
     if (result.statusCode == 200) {
       SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setString(Shared.accessToken, result.data['access_token']);
+      prefs.setString(Shared.accessToken, result.data['access']);
       await prefs.setBool('is_authenticated', true);
 
       return "Success";
