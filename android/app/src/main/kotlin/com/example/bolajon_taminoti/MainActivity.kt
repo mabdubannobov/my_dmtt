6fc7acd220000e455ef284f85e018eae05f02cc3
Ipilot7 <93022197+Ipilot7@users.noreply.github.com>
2025-08-26 12:48:09 +0400

    flutter sdk is updated

---
+++ android/app/src/main/kotlin/com/example/bolajon_taminoti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bolajon_taminoti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/my_dmtt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.valleycoders.bolajontaminoti
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
